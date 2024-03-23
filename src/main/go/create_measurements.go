31e55ad28796a64f75908fa2f8ac89400d30f5d4
Kawin Viriyaprasopsook <kawin.v@extend-it-resource.com>
2024-03-23 22:46:15 +0700

    📝 add descriptions

--- src/main/go/create_measurements.go
+++ src/main/go/create_measurements.go
@@ -1,3 +1,22 @@
+/*
+#  Copyright 2023 The original authors
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+*/
+
+// Based on https://github.com/gunnarmorling/1brc/blob/main/src/main/python/create_measurements.py
+
 package main
 
 import (
@@ -32,8 +51,7 @@ func buildWeatherStationNameList() []string {
 	stationNames := make([]string, 0)
 	file, err := os.Open("../../../data/weather_stations.csv")
 	if err != nil {
-		fmt.Println("Error opening file:", err)
-		os.Exit(1)
+		log.Panicln("Error opening file:", err)
 	}
 	defer file.Close()
 
@@ -45,8 +63,7 @@ func buildWeatherStationNameList() []string {
 		}
 	}
 	if err := scanner.Err(); err != nil {
-		fmt.Println("Error scanning file:", err)
-		os.Exit(1)
+		log.Panicln("Error scanning file:", err)
 	}
 
 	stationNamesMap := make(map[string]bool)
@@ -138,7 +155,7 @@ func buildTestData(weatherStationNames []string, numRowsToCreate int) {
 		}
 
 		// Print progress
-		if  progressInterval > 0 && chunk%progressInterval == 0 {
+		if progressInterval > 0 && chunk%progressInterval == 0 {
 			progress := (chunk * 100) / chunks
 			bars := strings.Repeat("=", progress/2)
 			fmt.Printf("\r[%-50s] %d%%", bars, progress)
